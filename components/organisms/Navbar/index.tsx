81907968edf5620c8707e319ed13d200601ff77e
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-06-28 22:09:02 +0700

    fix bug logo

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <Image src="/icon/logo.png" width={60} height={60} alt="" />
+            <Image src="/logo.svg" width={60} height={60} alt="" />
           </a>
           <ToggleMenu />
           <div className="collapse navbar-collapse" id="navbarNav">
