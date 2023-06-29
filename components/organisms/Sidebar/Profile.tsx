31f012a04a938a290f5f46c78199431b2d58df0d
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-06-29 14:31:39 +0700

    refactor: sidebar member

---
+++ components/organisms/Sidebar/Profile.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Profile() {
+  return (
+    <div className="user text-center pb-50 pe-30">
+      <Image src="/img/avatar-1.png" width={90} height={90} className="img-fluid mb-20" alt="" />
+      <h2 className="fw-bold text-xl color-palette-1 m-0">Olivia Rodrigo</h2>
+      <p className="color-palette-2 m-0">olivia@rodrigo.com</p>
+    </div>
+  );
+}
