8663a6e3374acfc741a64835732ba2aff3de49b1
rahmaynr <rahmaa55221@gmail.com>
2023-07-08 11:45:22 +0700

    Refactor member

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -1,26 +1,26 @@
-import Image from 'next/image';
-import cx from 'classnames';
-import Link from 'next/link';
+import Image from "next/image";
+import cx from "classnames";
+import Link from "next/link";
 
 interface MenuItemProps {
   title: string;
   icon:
-    | 'ic-menu-overview'
-    | 'ic-menu-transaction'
-    | 'ic-menu-message'
-    | 'ic-menu-card'
-    | 'ic-menu-reward'
-    | 'ic-menu-setting'
-    | 'ic-menu-logout';
+    | "ic-menu-overview"
+    | "ic-menu-transaction"
+    | "ic-menu-message"
+    | "ic-menu-card"
+    | "ic-menu-reward"
+    | "ic-menu-setting"
+    | "ic-menu-logout";
   active?: boolean;
-  href
+  href: string;
 }
 
 export default function MenuItem(props: Partial<MenuItemProps>) {
-  const { title, icon, active } = props;
+  const { title, icon, active, href } = props;
   const classItem = cx({
     item: true,
-    'mb-30': true,
+    "mb-30": true,
     active: active,
   });
   return (
@@ -29,13 +29,9 @@ export default function MenuItem(props: Partial<MenuItemProps>) {
         <Image src={`/icon/${icon}.svg`} width={25} height={25} alt="" />
       </div>
       <p className="item-title m-0">
-        <Link href={href}>
-
-        <a className="text-lg text-decoration-none"></a>
-        </Link>
-        
+        <Link href={href || ""} className="text-lg text-decoration-none">
           {title}
-        </a>
+        </Link>
       </p>
     </div>
   );
